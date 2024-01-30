9ffaeaf945228873ee1b4e4fbc40be4593b082ee
sirNewbies <104280247+sirnewbies@users.noreply.github.com>
2024-01-30 16:51:29 +0000

    topaz: Build overlay

--- device.mk
+++ device.mk
@@ -389,8 +389,9 @@ PRODUCT_PACKAGES += \
     WifiResCommon \
     WifiResTarget
 
-# Overlays Lineage
+# Overlays
 DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
 # Partitions
