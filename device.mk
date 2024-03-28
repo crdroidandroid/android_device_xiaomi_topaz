731e3938fd99d6ce06dbe42a39f680ecc44f520c
NikitaSavc <micraftur@gmail.com>
2024-03-28 11:08:58 +0300

    topaz: Drop init.qcom.post_boot.sh This script is used only by legacy SoCs. It is not needed for new platforms like bengal_515

--- device.mk
+++ device.mk
@@ -286,7 +286,6 @@ PRODUCT_PACKAGES += \
     init.kernel.post_boot.sh \
     init.qcom.class_core.sh \
     init.qcom.early_boot.sh \
-    init.qcom.post_boot.sh \
     init.qcom.sh \
     init.qti.dcvs.sh \
     init.qti.early_init.sh \
